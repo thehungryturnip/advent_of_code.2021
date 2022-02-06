e22ca2838060ac9bc8c2f76a05cabe1bf5add79e
The Hungry Turnip <thehungryturnip@gmail.com>
2022-02-06 04:36:46 -0500

    Solve day 2

---
+++ 2021_02.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import helpers
+
+course = helpers.get_input('2021_02')
+course = [ l.split(' ') for l in course]
+
+pos = 0
+depth = 0
+
+for c in course:
+    cmd = c[0]
+    amt = int(c[1])
+    match cmd:
+        case 'forward':
+            pos += amt
+        case 'down':
+            depth += amt
+        case 'up':
+            depth -= amt
+
+print(f'Part 1: {pos * depth}')
+
+pos = 0
+depth = 0
+aim = 0
+
+for c in course:
+    cmd = c[0]
+    amt = int(c[1])
+    match cmd:
+        case 'forward':
+            pos += amt
+            depth += aim * amt
+        case 'down':
+            aim += amt
+        case 'up':
+            aim -= amt
+
+print(f'Part 2: {pos * depth}')
