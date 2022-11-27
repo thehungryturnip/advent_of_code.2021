18a6f3a244259f459e16b52fe653cc59f114a141
The Hungry Turnip <thehungryturnip@weisoflife.com>
2022-11-26 23:57:32 -0500

    Complete day 10 pant 2

--- 2021_10.py
+++ 2021_10.py
@@ -14,11 +14,18 @@ class NavSys(list):
 
     close = {")", "]", "}", ">"}
 
-    points = {
+    syntax_error_points = {
         ")": 3,
         "]": 57,
         "}": 1197,
         ">": 25137
+    }
+
+    autocomplete_points = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4
     }
 
     def __init__(self, data):
@@ -30,6 +37,11 @@ class NavSys(list):
     def sum_syntax_error_score(self):
         return sum([NavSys._calc_syntax_error_score(e) for e in self])
 
+    def calc_middle_autocomplete_score(self):
+        all_scores = [NavSys._calc_autocomplete_score(e) for e in self]
+        autocomplete_scores = sorted([s for s in all_scores if not s == 0])
+        return autocomplete_scores[len(autocomplete_scores) // 2]
+
     @classmethod
     def _calc_syntax_error_score(cls, entry):
         tracker = deque([])
@@ -39,13 +51,36 @@ class NavSys(list):
                 tracker.append(cls.brackets[c])
             elif c in cls.close:
                 if not c == tracker.pop():
-                    return cls.points[c]
+                    return cls.syntax_error_points[c]
 
         return 0
 
+    @classmethod
+    def _calc_autocomplete_score(cls, entry):
+        tracker = deque([])
 
-data = get_input('2021_10', False)
+        for c in entry:
+            if c in cls.brackets:
+                tracker.append(cls.brackets[c])
+            elif c in cls.close:
+                if not c == tracker[-1]:
+                    return 0
+                else:
+                    tracker.pop()
+
+        score = 0
+        while tracker:
+            score = score * 5
+            c = tracker.pop()
+            score = score + cls.autocomplete_points[c]
+
+        return score
+
+
+data = get_input('2021_10')
 sys = NavSys(data)
 
 print(
-    f'Pant 1: The total syntax error score is {sys.sum_syntax_error_score()}')
+    f'Part 1: The total syntax error score is {sys.sum_syntax_error_score()}')
+print(
+    f'Part 2: The middle autocomplete score is {sys.calc_middle_autocomplete_score()}')
