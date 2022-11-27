7136a10a4fea533dd06f409dd8650cc0d20c6040
The Hungry Turnip <thehungryturnip@weisoflife.com>
2022-11-26 22:09:19 -0500

    Solve day 9

---
+++ 2021_09.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from collections import deque
+from functools import reduce
+from helpers import get_input, Point, RectMap
+
+class DepthMap(RectMap):
+
+    def __init__(self, data):
+        super().__init__(data);
+
+    def __repr__(self):
+        return super().__repr__()
+
+    def sum_low_points(self):
+        low_points = list(filter(lambda p: self._is_low_point(p), self))
+        return sum([self[p] + 1 for p in low_points])
+
+    def multiply_basin_sizes(self):
+        low_points = list(filter(lambda p: self._is_low_point(p), self))
+        basin_sizes = list(map(lambda p: self._basin_size(p), low_points))
+        basin_sizes.sort()
+        biggest_3 = basin_sizes[-3:]
+        return reduce(lambda x, y: x * y, biggest_3)
+
+    def _is_low_point(self, point):
+        neighbors = self.get_neighbors(point)
+        higher = [self[neighbor] > self[point] for neighbor in neighbors]
+        return all(higher)
+
+    def _basin_size(self, point):
+        basin_points = {point}
+        to_check = deque([point])
+
+        while (to_check):
+            p = to_check.pop()
+            neighbors = self.get_neighbors(p)
+
+            for n in neighbors:
+                if (self[n] == 9):
+                    continue
+                if (not n in basin_points):
+                    to_check.append(n)
+                    basin_points.add(n)
+
+        return len(basin_points)
+
+
+
+data = get_input('2021_09')
+map_ = DepthMap(data)
+
+print(f'Pant 1: Risk level is {map_.sum_low_points()}')
+print(f'Part 2: Multiplied basin size is {map_.multiply_basin_sizes()}')
--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 def get_input(file, ex=False):
     if ex:
         file = f'{file}_ex'
@@ -10,3 +12,41 @@ def get_input(file, ex=False):
                 return output
             output.append(line)
         return output
+
+Point = namedtuple("Point", "r c")
+
+class RectMap(dict):
+
+    def __init__(self, data):
+        self.height = len(data)
+        self.width = len(data[0])
+        for r, row in enumerate(data):
+            for c, v in enumerate(row):
+                self[Point(r, c)] = int(v)
+
+    def __repr__(self):
+        return "\n".join([
+            "".join([str(self[Point(r, c)]) for c in range(self.width)])
+            for r
+            in range(self.height)
+        ])
+
+    def get_neighbors(self, p):
+        neighbors = [
+            Point(p.r - 1, p.c),
+            Point(p.r + 1, p.c),
+            Point(p.r, p.c - 1),
+            Point(p.r, p.c + 1),
+        ]
+
+        return list(filter(lambda p: not self.out_of_bounds(p), neighbors))
+
+    def out_of_bounds(self, p):
+        if (p.r < 0 or p.c < 0):
+            return True
+        if (p.r >= self.height):
+            return True
+        if (p.c >= self.width):
+            return True
+        return False
+
